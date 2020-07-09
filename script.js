3484fd2c5c0c8e218bc3b2f33b080fc4ba384830
Corey Burkett <coreyburkett22@gmail.com>
2020-07-08 23:53:00 -0700

    Most recent update

--- script.js
+++ script.js
@@ -1,162 +1,207 @@
-let secondsLeft = 75;
+let secondsLeft = 10;
 let timeEl = document.querySelector('#timer');
 let startBtn = document.querySelector('#startBtn');
 
-let incorrectAnsEl = document.getElementsByClassName('incorrect-1');
-let qTwoEl = document.getElementById('question-2');
-let choicesEl = document.getElementsByClassName('btnLayout');
-let correctBtn = document.getElementsByTagName('button')[2].getAttribute('data-correct');
-let incorrectBtn = document.getElementsByTagName('button')[3].getAttribute('data-incorrect');
-console.log(correctBtn);
-
+// Home page and start button
 startBtn.addEventListener('click', function startQuiz() {
     let timerInterval = setInterval(function() {
         secondsLeft--;
-        timeEl.textContent = "Time Left: " + secondsLeft;
+        timeEl.textContent = 'Time Left: ' + secondsLeft;
 
+        // Skip to score page if timer runs out
         if (secondsLeft === 0) {
             clearInterval(timerInterval);
-            alert("TIMES UP! Let's see how you did.");
+            alert('TIMES UP! You answered ' + score + ' question(s) correctly.');
+            qOneEl.style.display = 'none';
+            qTwoEl.style.display = 'none';
+            qThreeEl.style.display = 'none';
+            qFourEl.style.display = 'none';
+            qFiveEl.style.display = 'none';
+            scoreEl.style.display = 'contents'
         }
     }, 1000);
 
-    // QUESTION 1
     let startEl = document.getElementById('start');
     let qOneEl = document.getElementById('question-1');
+    let qTwoEl = document.getElementById('question-2');
+    let qThreeEl = document.getElementById('question-3');
+    let qFourEl = document.getElementById('question-4');
+    let qFiveEl = document.getElementById('question-5');
+    let scoreEl = document.getElementById('score');
+    let score = 0;
 
-    startEl.style.display = "none";
-    qOneEl.style.display = "contents";
+    // When startBtn is pressed, the start page will hide while the first question shows
+    startEl.style.display = 'none';
+    qOneEl.style.display = 'contents';
 
-    document.getElementById('p1').innerHTML = "1. A _______ is an error in a program that prevents the program from running as expected.";
-    document.getElementById('q1-c1').innerHTML = "<p>A. Array</p>";
-    document.getElementById('q1-c2').innerHTML = "<p>B. Bug</p>";
-    document.getElementById('q1-c3').innerHTML = "<p>C. Variable</p>";
-    document.getElementById('q1-c4').innerHTML = "<p>D. For Loop</p>";
+    // QUESTION 1
+    document.getElementById('p1').innerHTML = '1. A _______ is an error in a program that prevents the program from running as expected.';
+    document.getElementById('q1-c1').innerHTML = '<p>A. Array</p>';
+    document.getElementById('q1-c2').innerHTML = '<p>B. Bug</p>';
+    document.getElementById('q1-c3').innerHTML = '<p>C. Variable</p>';
+    document.getElementById('q1-c4').innerHTML = '<p>D. For Loop</p>';
     
-    // let correctAnsEl = document.getAttribute('data-correct');
+    document.getElementById('q1-c1').onclick = function correctOne() {
+        score--;
+        alert('Sorry, that was incorrect.');
+        qOneEl.style.display = 'none';
+        qTwoEl.style.display = 'contents';
+     };
+    document.getElementById('q1-c2').onclick = function() {
+        score++;
+        alert('CORRECT!');
+        qOneEl.style.display = 'none';
+        qTwoEl.style.display = 'contents';
+     };
+    document.getElementById('q1-c3').onclick = function() {
+        score--;
+        alert('Sorry, that was incorrect.');
+        qOneEl.style.display = 'none';
+        qTwoEl.style.display = 'contents';
+     };
+    document.getElementById('q1-c4').onclick = function() {
+        score--;
+        alert('Sorry, that was incorrect.');
+        qOneEl.style.display = 'none';
+        qTwoEl.style.display = 'contents';
+     };
 
-    // Issues:
-    // - Parent/ child relationship inside HTML with div and "choice" buttons
-    // - Asynchronous behaivor (line 43) .addEventListener is not registering as a function, but choicesEl IS working.
-    // - I want "data-correct" to be the correct choices and "data-incorrect" will be incorrect choices
-    
-     console.log(correctBtn);
+    // QUESTION 2
+    document.getElementById('p2').innerHTML = '2. What does HTML stand for?';
+    document.getElementById('q2-c1').innerHTML = '<p>A. Hyper Text Markup Language</p>';
+    document.getElementById('q2-c2').innerHTML = '<p>B. Home Tool Markup Language</p>';
+    document.getElementById('q2-c3').innerHTML = '<p>C. Hyperlinks and Text Markup Language</p>';
+    document.getElementById('q2-c4').innerHTML = '<p>D. He Texted My Lady</p>';
 
-    choicesEl.addEventListener('click', function() {
-        console.log(correctBtn);
-        if (correctBtn) {
-            alert('CORRECT!');
-            qOneEl.style.display = "none";
-            qTwoEl.style.display = "contents";
-        } else {
-            alert('Sorry, that was incorrect.');
-            qOneEl.style.display = "none";
-            qTwoEl.style.display = "contents";
+    document.getElementById('q2-c1').onclick = function() {
+        score++;
+        alert('CORRECT!');
+        qTwoEl.style.display = 'none';
+        qThreeEl.style.display = 'contents';
+     };
+    document.getElementById('q2-c2').onclick = function() {
+        score--;
+        alert('Sorry, that was incorrect.');
+        qTwoEl.style.display = 'none';
+        qThreeEl.style.display = 'contents';
+     };
+    document.getElementById('q2-c3').onclick = function() {
+        score--;
+        alert('Sorry, that was incorrect.');
+        qTwoEl.style.display = 'none';
+        qThreeEl.style.display = 'contents';
+     };
+    document.getElementById('q2-c4').onclick = function() {
+        score--;
+        alert('Sorry, that was incorrect.');
+        qTwoEl.style.display = 'none';
+        qThreeEl.style.display = 'contents';
+     };
+
+    // QUESTION 3
+    document.getElementById('p3').innerHTML = '3. A(n) _______ is a placeholder for a piece of information that can change.';
+    document.getElementById('q3-c1').innerHTML = '<p>A. Conditional</p>';
+    document.getElementById('q3-c2').innerHTML = '<p>B. Digital Footprint</p>';
+    document.getElementById('q3-c3').innerHTML = '<p>C. IP Address</p>';
+    document.getElementById('q3-c4').innerHTML = '<p>D. Variable</p>';
+
+    document.getElementById('q3-c1').onclick = function() {
+        score--;
+        alert('Sorry, that was incorrect.');
+        qThreeEl.style.display = 'none';
+        qFourEl.style.display = 'contents';
+     };
+    document.getElementById('q3-c2').onclick = function() {
+        score--;
+        alert('Sorry, that was incorrect.');
+        qThreeEl.style.display = 'none';
+        qFourEl.style.display = 'contents';
+     };
+    document.getElementById('q3-c3').onclick = function() {
+        score--;
+        alert('Sorry, that was incorrect.');
+        qThreeEl.style.display = 'none';
+        qFourEl.style.display = 'contents';
+     };
+    document.getElementById('q3-c4').onclick = function() {
+        score++;
+        alert('CORRECT!');
+        qThreeEl.style.display = 'none';
+        qFourEl.style.display = 'contents';
+     };
+
+    // QUESTION 4
+    document.getElementById('p4').innerHTML = '4. A(n) _______ is a structure in code where you perform the same action or actions multiple times in a row.';
+    document.getElementById('q4-c1').innerHTML = '<p>A. Loop</p>';
+    document.getElementById('q4-c2').innerHTML = '<p>B. console.log</p>';
+    document.getElementById('q4-c3').innerHTML = '<p>C. if statement</p>';
+    document.getElementById('q4-c4').innerHTML = '<p>D. Object</p>';
+
+    document.getElementById('q4-c1').onclick = function() {
+        score++;
+        alert('CORRECT!');
+        qFourEl.style.display = 'none';
+        qFiveEl.style.display = 'contents';
+     };
+    document.getElementById('q4-c2').onclick = function() {
+        score--;
+        alert('Sorry, that was incorrect.');
+        qFourEl.style.display = 'none';
+        qFiveEl.style.display = 'contents';
+     };
+    document.getElementById('q4-c3').onclick = function() {
+        score--;
+        alert('Sorry, that was incorrect.');
+        qFourEl.style.display = 'none';
+        qFiveEl.style.display = 'contents';
+     };
+    document.getElementById('q4-c4').onclick = function() {
+        score--;
+        alert('Sorry, that was incorrect.');
+        qFourEl.style.display = 'none';
+        qFiveEl.style.display = 'contents';
+     };
+
+    // QUESTION 5
+    document.getElementById('p5').innerHTML = '5. A(n) _______ has properties, also known as keys, to which are assigned values.';
+    document.getElementById('q5-c1').innerHTML = '<p>A. Conditional</p>';
+    document.getElementById('q5-c2').innerHTML = '<p>B. Function</p>';
+    document.getElementById('q5-c3').innerHTML = '<p>C. Object</p>';
+    document.getElementById('q5-c4').innerHTML = '<p>D. Array</p>';
+
+    document.getElementById('q5-c1').onclick = function() {
+        score--;
+        alert('Sorry, that was incorrect.');
+        qFiveEl.style.display = 'none';
+        scoreEl.style.display = 'contents';
+     };
+    document.getElementById('q5-c2').onclick = function() {
+        score--;
+        alert('Sorry, that was incorrect.');
+        qFiveEl.style.display = 'none';
+        scoreEl.style.display = 'contents';
+     };
+    document.getElementById('q5-c3').onclick = function() {
+        score++;
+        alert('CORRECT!');
+        qFiveEl.style.display = 'none';
+        scoreEl.style.display = 'contents';
+     };
+    document.getElementById('q5-c4').onclick = function() {
+        score--;
+        alert('Sorry, that was incorrect.');
+        qFiveEl.style.display = 'none';
+        scoreEl.style.display = 'contents';
+     };
+
+    // SCORE & add score to the local storage
+    let enterScore = document.getElementById('textbox');
+
+    enterScore.addEventListener("keydown", function(event) {
+        if (event.keyCode === 13) {
+            console.log("Enter key was pressed");
+            event.preventDefault()
         }
     });
 
-    // choicesEl.addEventListener('click', function() {
-    //     if (incorrectAnsEl = true) {
-    //         console.log(incorrectAnsEl);
-    //         alert('Sorry, that was incorrect.');
-    //         qOneEl.style.display = "none";
-    //         qTwoEl.style.display = "contents";
-    //     }
-    // });
-
-    // QUESTION 2
-    document.getElementById('p2').innerHTML = "2. What does HTML stand for?";
-    document.getElementById('q2-c1').innerHTML = "<p>A. Hyper Text Markup Language</p>";
-    document.getElementById('q2-c2').innerHTML = "<p>B. Home Tool Markup Language</p>";
-    document.getElementById('q2-c3').innerHTML = "<p>C. Hyperlinks and Text Markup Language</p>";
-    document.getElementById('q2-c4').innerHTML = "<p>D. He Texted My Lady</p>";
-
-    let correctAns2 = document.getElementById('q2-c1');
-    let qThreeEl = document.getElementById('question-3');
-
-    correctAns2.addEventListener('click', function() {
-        alert("CORRECT!");
-        qTwoEl.style.display = "none";
-        qThreeEl.style.display = "contents";
-    });
-
-    // QUESTION 3
-    document.getElementById('p3').innerHTML = "3. A(n) _______ is a placeholder for a piece of information that can change.";
-    document.getElementById('q3-c1').innerHTML = "<p>A. Conditional</p>";
-    document.getElementById('q3-c2').innerHTML = "<p>B. Digital Footprint</p>";
-    document.getElementById('q3-c3').innerHTML = "<p>C. IP Address</p>";
-    document.getElementById('q3-c4').innerHTML = "<p>D. Variable</p>";
-
-    let correctAns3 = document.getElementById('q3-c4');
-    let qFourEl = document.getElementById('question-4');
-
-    correctAns3.addEventListener('click', function() {
-        alert("CORRECT!");
-        qThreeEl.style.display = "none";
-        qFourEl.style.display = "contents";
-    });
-
-    // QUESTION 4
-    document.getElementById('p4').innerHTML = "4. ";
-    document.getElementById('q4-c1').innerHTML = "<p>A. C</p>";
-    document.getElementById('q4-c2').innerHTML = "<p>B. </p>";
-    document.getElementById('q4-c3').innerHTML = "<p>C. </p>";
-    document.getElementById('q4-c4').innerHTML = "<p>D. </p>";
-
-    let correctAns4 = document.getElementById('q4-c1');
-    let qFiveEl = document.getElementById('question-5');
-
-    correctAns4.addEventListener('click', function() {
-        alert("CORRECT!");
-        qFourEl.style.display = "none";
-        qFiveEl.style.display = "contents";
-    });
-
-    // QUESTION 5
-    document.getElementById('p5').innerHTML = "5. ";
-    document.getElementById('q5-c1').innerHTML = "<p>A. </p>";
-    document.getElementById('q5-c2').innerHTML = "<p>B. </p>";
-    document.getElementById('q5-c3').innerHTML = "<p>C. C</p>";
-    document.getElementById('q5-c4').innerHTML = "<p>D. </p>";
-
-    let correctAns5 = document.getElementById('q5-c3');
-    let scoreEl = document.getElementById('score');
-
-    correctAns5.addEventListener('click', function() {
-        alert("CORRECT!");
-        qFourEl.style.display = "none";
-        scoreEl.style.display = "contents";
-    });
-
 });
-
-
-
-
-// let questions = [
-//     {
-//         question: "1. A _______ is an error in a program that prevents the program from running as expected.",
-//         choices: ["A", "B", "C", "D"],
-//         correctAns: "B"
-//     },
-//     {
-//         question: "2. What does a for loop do?",
-//         choices: ["A", "B", "C", "D"],
-//         correctAns: "C"
-//     },
-//     {
-//         question: "3. A _______ is a placeholder for a piece of information that can change.",
-//         choices: ["A", "B", "C", "D"],
-//         correctAns: "A"
-//     },
-//     {
-//         question: "",
-//         choices: ["A", "B", "C", "D"],
-//         correctAns: "C"
-//     },
-//     {
-//         question: "",
-//         choices: ["A", "B", "C", "D"],
-//         correctAns: "D"
-//     }
-//     ];
